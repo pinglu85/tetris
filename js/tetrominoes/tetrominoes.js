907add6fb47de32b97ed210e07b267058ffd60f4
pinglu85 <luping8576@hotmail.com>
2020-05-25 23:31:00 +0200

    fix _lTetromino rotation issue

--- js/tetrominoes/tetrominoes.js
+++ js/tetrominoes/tetrominoes.js
@@ -9,7 +9,7 @@ export default function tetrominoes(width) {
   ];
 
   const _lTetromino = [
-    [width + 2, width * 2, width * 2 + 1, width * 2 + 2],
+    [width * 2, width * 2 + 1, width + 2, width * 2 + 2],
     // [2, width, width + 1, width + 2],
     [0, 1, width + 1, width * 2 + 1],
     [width, width + 1, width + 2, width * 2],
