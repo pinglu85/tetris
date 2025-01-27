c0375dae42378726cccc4568d0cfec34a1197c2e
pinglu85 <luping8576@hotmail.com>
2025-01-28 00:58:58 +0100

    chore: set up Vite + TypeScript and reorganize folder structure

---
+++ src/colors/colors.js
@@ -0,0 +1,19 @@
+export const colors = [
+  "url(/blue_block.png)",
+  "url(/green_block.png)",
+  "url(/navy_block.png)",
+  "url(/peach_block.png)",
+  "url(/pink_block.png)",
+  "url(/purple_block.png)",
+  "url(/yellow_block.png)",
+];
+
+export const svgColors = [
+  "#6AC4E5",
+  "#5dcfa5",
+  "#116B8B",
+  "#FFA28D",
+  "#D48DC3",
+  "#755AC3",
+  "#BFC292",
+];
--- src/main.js
+++ src/main.js
@@ -1,18 +1,45 @@
-import createTetrominoes from './tetrominoes/tetrominoes.js';
-import { smallTetrominoes } from './tetrominoes/smallTetrominoes.js';
-import { colors, svgColors } from './colors/colors.js';
-import generateRandomIndex from './utils/generateRandomIndex.js';
+import createTetrominoes from "./tetrominoes/tetrominoes.js";
+import { smallTetrominoes } from "./tetrominoes/smallTetrominoes.js";
+import { colors, svgColors } from "./colors/colors.js";
+import generateRandomIndex from "./utils/generateRandomIndex.js";
+
+import "./style.css";
 
 const GRID_WIDTH = 10;
 const GRID_HEIGHT = 20;
 const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT;
 
+document.getElementById("app").innerHTML = `
+  <header class="header">
+      <h1>Tetris</h1>
+      <button class="btn" id="start-button">Start / Pause</button>
+  </header>
+  <main class="game-wrapper">
+      <div class="game-info">
+          <div class="score-wrapper">
+              <p class="score-label">Score</p>
+              <p id="score">0</p>
+          </div>
+          <div>
+              Next
+              <div id="next-tetromino-wrapper">
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 100 100"
+                  ></svg>
+              </div>
+          </div>
+      </div>
+      <div id="game-grid"></div>
+  </main>
+`;
+
 const gameGrid = createGrid(GRID_SIZE, GRID_WIDTH);
-const cells = Array.from(gameGrid.querySelectorAll('div'));
+const cells = Array.from(gameGrid.querySelectorAll("div"));
 const tetrominoes = createTetrominoes(GRID_WIDTH);
-const nextTetrominoSVG = document.querySelector('#next-tetromino-wrapper svg');
-const startButton = document.getElementById('start-button');
-const score = document.getElementById('score');
+const nextTetrominoSVG = document.querySelector("#next-tetromino-wrapper svg");
+const startButton = document.getElementById("start-button");
+const score = document.getElementById("score");
 const width = 10;
 
 let gameStart = false;
@@ -21,14 +48,14 @@ let isDescending;
 
 // Create grid
 function createGrid(size, width) {
-  const grid = document.getElementById('game-grid');
+  const grid = document.getElementById("game-grid");
   // Main grid
   for (let i = 0; i < size; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
     // Set bottom boundary
     if (i >= size - width) {
-      cell.classList.add('bottom');
+      cell.classList.add("bottom");
     }
     grid.appendChild(cell);
   }
@@ -49,16 +76,16 @@ function drawCurrent(tetromino, colorIndex) {
 function undrawCurrent(tetromino) {
   tetromino.forEach((index) => {
     // cells[index + currentPosition].classList.remove('block');
-    cells[index + currentPosition].style.backgroundImage = '';
+    cells[index + currentPosition].style.backgroundImage = "";
   });
 }
 
 // Display next Tetromino
 function drawNext(smallTetromino, colorIndex) {
-  const xmlns = 'http://www.w3.org/2000/svg';
-  const path = document.createElementNS(xmlns, 'path');
-  path.setAttribute('fill', `${svgColors[colorIndex]}`);
-  path.setAttribute('d', smallTetromino);
+  const xmlns = "http://www.w3.org/2000/svg";
+  const path = document.createElementNS(xmlns, "path");
+  path.setAttribute("fill", `${svgColors[colorIndex]}`);
+  path.setAttribute("d", smallTetromino);
 
   nextTetrominoSVG.appendChild(path);
 }
@@ -81,7 +108,7 @@ let nextTetromino;
 let nextColorIndex;
 
 // Start or pause the game
-startButton.addEventListener('click', () => {
+startButton.addEventListener("click", () => {
   gameStart = !gameStart;
   if (gameStart) {
     if (!currentTetromino && !nextTetromino) {
@@ -111,16 +138,16 @@ function descend() {
     // Check if the Tetromino reaches the bottom or lands on the previous piece
     const reachBottom = currentTetromino.some(
       (index) =>
-        cells[index + currentPosition].classList.contains('bottom') ||
+        cells[index + currentPosition].classList.contains("bottom") ||
         cells[index + currentPosition + width].classList.contains(
-          'previousPiece'
-        )
+          "previousPiece",
+        ),
     );
 
     if (reachBottom) {
       clearInterval(isDescending);
       currentTetromino.forEach((index) =>
-        cells[index + currentPosition].classList.add('previousPiece')
+        cells[index + currentPosition].classList.add("previousPiece"),
       );
       calculateScore();
       currentTetrominoIndex = nextTetrominoIndex;
@@ -149,16 +176,16 @@ function descend() {
 // Game controls
 function control(e) {
   switch (e.key) {
-    case 'ArrowRight':
+    case "ArrowRight":
       moveRight();
       break;
-    case 'ArrowLeft':
+    case "ArrowLeft":
       moveLeft();
       break;
-    case 'ArrowDown':
+    case "ArrowDown":
       moveDown();
       break;
-    case 'ArrowUp':
+    case "ArrowUp":
       rotation();
       break;
     default:
@@ -167,13 +194,13 @@ function control(e) {
 }
 
 // Listen for key down
-document.addEventListener('keydown', control);
+document.addEventListener("keydown", control);
 
 function moveRight() {
   const rightmost = currentTetromino.some(
     (index) =>
       (index + currentPosition + 1) % 10 === 0 ||
-      cells[index + currentPosition + 1].classList.contains('previousPiece')
+      cells[index + currentPosition + 1].classList.contains("previousPiece"),
   );
   if (!rightmost) {
     undrawCurrent(currentTetromino);
@@ -186,7 +213,7 @@ function moveLeft() {
   const leftmost = currentTetromino.some(
     (index) =>
       (index + currentPosition) % 10 === 0 ||
-      cells[index + currentPosition - 1].classList.contains('previousPiece')
+      cells[index + currentPosition - 1].classList.contains("previousPiece"),
   );
   if (!leftmost) {
     undrawCurrent(currentTetromino);
@@ -198,8 +225,10 @@ function moveLeft() {
 function moveDown() {
   const bottom = currentTetromino.some(
     (index) =>
-      cells[index + currentPosition].classList.contains('bottom') ||
-      cells[index + currentPosition + width].classList.contains('previousPiece')
+      cells[index + currentPosition].classList.contains("bottom") ||
+      cells[index + currentPosition + width].classList.contains(
+        "previousPiece",
+      ),
   );
   if (!bottom) {
     undrawCurrent(currentTetromino);
@@ -210,11 +239,11 @@ function moveDown() {
 
 function rotation() {
   const leftmost = currentTetromino.some(
-    (index) => (index + currentPosition) % 10 === 0
+    (index) => (index + currentPosition) % 10 === 0,
   );
   const rightmost =
     currentTetromino.some(
-      (index) => (index + currentPosition + 1) % 10 === 0
+      (index) => (index + currentPosition + 1) % 10 === 0,
     ) ||
     currentTetromino.some((index) => (index + currentPosition + 2) % 10 === 0);
   const rotation =
@@ -231,15 +260,15 @@ function rotation() {
   let isCollidedBottom = false;
   if (
     (leftmost && (leftmostBlock + tempPosition) % width === 9) ||
-    cells[leftmostBlock + tempPosition].classList.contains('previousPiece')
+    cells[leftmostBlock + tempPosition].classList.contains("previousPiece")
   ) {
     tempPosition++;
     if (
       cells[rightmostBlockOne + tempPosition].classList.contains(
-        'previousPiece'
+        "previousPiece",
       ) ||
       cells[rightmostBlockTwo + tempPosition].classList.contains(
-        'previousPiece'
+        "previousPiece",
       )
     ) {
       isCollidedLeft = true;
@@ -249,7 +278,7 @@ function rotation() {
     if (
       (rightmost && (rightmostBlockOne + tempPosition) % width <= 1) ||
       cells[rightmostBlockOne + tempPosition].classList.contains(
-        'previousPiece'
+        "previousPiece",
       )
     ) {
       tempPosition--;
@@ -257,21 +286,21 @@ function rotation() {
   }
   if (
     (rightmost && (rightmostBlockTwo + tempPosition) % width === 0) ||
-    cells[rightmostBlockTwo + tempPosition].classList.contains('previousPiece')
+    cells[rightmostBlockTwo + tempPosition].classList.contains("previousPiece")
   ) {
     tempPosition--;
   }
-  if (cells[leftmostBlock + tempPosition].classList.contains('previousPiece')) {
+  if (cells[leftmostBlock + tempPosition].classList.contains("previousPiece")) {
     isCollidedRight = true;
   }
   for (let i = 0; i < 2; i++) {
     if (
       bottommostBlock + tempPosition >= GRID_SIZE ||
-      cells[bottommostBlock + tempPosition].classList.contains('previousPiece')
+      cells[bottommostBlock + tempPosition].classList.contains("previousPiece")
     ) {
       tempPosition -= 10;
       if (
-        cells[topmostBlock + tempPosition].classList.contains('previousPiece')
+        cells[topmostBlock + tempPosition].classList.contains("previousPiece")
       ) {
         isCollidedBottom = true;
       }
@@ -290,20 +319,20 @@ function calculateScore() {
   for (let i = 0; i < GRID_SIZE; i += 10) {
     const line = cells.slice(i, i + width);
     const completedLine = line.every((cell) =>
-      cell.classList.contains('previousPiece')
+      cell.classList.contains("previousPiece"),
     );
     if (completedLine) {
       line.forEach((cell) => {
-        cell.style.backgroundImage = '';
-        cell.classList.remove('previousPiece');
+        cell.style.backgroundImage = "";
+        cell.classList.remove("previousPiece");
       });
       for (let index = i - 1; index >= 0; index--) {
-        if (cells[index].classList.contains('previousPiece')) {
-          cells[index + 10].classList.add('previousPiece');
+        if (cells[index].classList.contains("previousPiece")) {
+          cells[index + 10].classList.add("previousPiece");
           cells[index + 10].style.backgroundImage =
             cells[index].style.backgroundImage;
-          cells[index].classList.remove('previousPiece');
-          cells[index].style.backgroundImage = '';
+          cells[index].classList.remove("previousPiece");
+          cells[index].style.backgroundImage = "";
         }
       }
       score.innerText = String(Number(score.innerText) + 1);
