5b12ec28f2f04367c1ab28278b16b32a9cb2c5cc
pinglu85 <luping8576@hotmail.com>
2025-03-18 13:32:02 +0100

    test(board): add unit test for `Board.createEmpty()` method

---
+++ src/models/Board.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest';
+
+import { Board } from './Board';
+import { NUM_OF_BUFFER_ROWS } from '../constants';
+
+import type { Cell } from './Board';
+
+describe('Board', () => {
+  describe('createEmpty', () => {
+    it('creates a board with an empty grid', () => {
+      const numOfRows = 20;
+      const numOfCols = 10;
+      const board = Board.createEmpty(numOfRows, numOfCols);
+      const expectedGrid: Cell[][] = Array.from(
+        { length: numOfRows + NUM_OF_BUFFER_ROWS },
+        () =>
+          Array.from({ length: numOfCols }, () => ({
+            filled: false,
+            color: '',
+          }))
+      );
+      expect(board.grid).toStrictEqual(expectedGrid);
+    });
+  });
+});
