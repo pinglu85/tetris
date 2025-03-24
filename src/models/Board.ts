19d725f23ab7f6ddaced59d7db9ff73bc55ed993
pinglu85 <luping8576@hotmail.com>
2025-03-24 12:16:44 +0100

    Revert "refactor(models): require input grid to include buffer rows"
    This reverts commit e897bc5d898a263326c95afeac8e7e5e59747c76.

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -33,18 +33,26 @@ export class Board {
   }
 
   static fromGrid(originalGrid: Cell[][]): Board {
-    const numOfRows = originalGrid.length;
-    if (numOfRows <= NUM_OF_BUFFER_ROWS) {
-      throw new Error('The grid must include buffer rows.');
-    }
-
+    const numOfRows = originalGrid.length + NUM_OF_BUFFER_ROWS;
     const numOfCols = originalGrid[0].length;
-    const grid: Cell[][] = Array.from({ length: numOfRows }, (_, i) =>
-      Array.from({ length: numOfCols }, (_, j) => ({
-        filled: originalGrid[i][j].filled,
-        color: originalGrid[i][j].color,
-      }))
-    );
+    const grid: Cell[][] = [];
+
+    for (let i = 0; i < numOfRows; i++) {
+      const row: Cell[] = new Array(numOfCols);
+
+      for (let j = 0; j < numOfCols; j++) {
+        if (i < NUM_OF_BUFFER_ROWS) {
+          row[j] = { filled: false, color: '' };
+        } else {
+          row[j] = {
+            filled: originalGrid[i - NUM_OF_BUFFER_ROWS][j].filled,
+            color: originalGrid[i - NUM_OF_BUFFER_ROWS][j].color,
+          };
+        }
+      }
+
+      grid.push(row);
+    }
 
     return new Board(grid);
   }
