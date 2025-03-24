2ad62594e581f348ac5e5e1a6ebe40c8ee5deebb
pinglu85 <luping8576@hotmail.com>
2025-03-24 16:40:58 +0100

    Revert "test(board): add unit test for `Board.fromGrid` method"
    This reverts commit 9f919f885536b0c3186ed5a0fc25b772db185121.

--- src/models/Board.test.ts
+++ src/models/Board.test.ts
@@ -5,27 +5,6 @@ import { NUM_OF_BUFFER_ROWS } from '../constants';
 
 import type { Cell } from './Board';
 
-enum Colors {
-  BLUE = 'blue',
-  GREEN = 'green',
-  NAVY = 'navy',
-  PINK = 'pink',
-  YELLOW = 'yellow',
-  RED = 'red',
-  WHITE = 'white',
-}
-
-const CHARACTER_TO_CELL_STATE: Record<string, Cell> = {
-  '.': { filled: false, color: '' },
-  B: { filled: true, color: Colors.BLUE },
-  G: { filled: true, color: Colors.GREEN },
-  N: { filled: true, color: Colors.NAVY },
-  P: { filled: true, color: Colors.PINK },
-  Y: { filled: true, color: Colors.YELLOW },
-  R: { filled: true, color: Colors.RED },
-  W: { filled: true, color: Colors.WHITE },
-};
-
 describe('Board', () => {
   describe('createEmpty', () => {
     it('creates a board with an empty grid', () => {
@@ -45,41 +24,4 @@ describe('Board', () => {
       expect(board.grid).toStrictEqual(expectedGrid);
     });
   });
-
-  describe('fromGrid', () => {
-    it('creates a board from a given grid', () => {
-      const grid = `
-        . . W . . . . . . .
-        . . W . . . . . . .
-        . . W W . . . . Y Y
-        G . N . . R R . Y Y
-        G N N N . R R Y Y .
-        G G . P P P P . Y Y
-      `;
-      const board = Board.fromGrid(stringToGrid(grid));
-
-      expect(board).toBeInstanceOf(Board);
-      expect(board.grid).toStrictEqual(stringToGrid(grid, true));
-    });
-  });
 });
-
-function stringToGrid(gridString: string, addBufferRows = false): Cell[][] {
-  const grid = gridString
-    .trim()
-    .split('\n')
-    .map((row) =>
-      row
-        .trim()
-        .split(' ')
-        .map((char) => CHARACTER_TO_CELL_STATE[char])
-    );
-
-  if (!addBufferRows) return grid;
-
-  const bufferRows = Array.from({ length: NUM_OF_BUFFER_ROWS }, () =>
-    Array.from({ length: grid[0].length }, () => CHARACTER_TO_CELL_STATE['.'])
-  );
-
-  return [...bufferRows, ...grid];
-}
