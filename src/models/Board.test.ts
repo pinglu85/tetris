4d59a4b2a0a78332d56627c06afaa0f62c7fe11d
pinglu85 <luping8576@hotmail.com>
2025-06-02 14:23:57 +0200

    test(board): add explicit type annotation to tetromino initialization

--- src/models/Board.test.ts
+++ src/models/Board.test.ts
@@ -287,7 +287,7 @@ describe('Board', () => {
             `,
         BUFFER_ROW_COUNT
       );
-      const tetromino = {
+      const tetromino: TetrominoLike = {
         color: Colors.BLUE,
         isLocked: true,
         getBlockPositions: mockGetBlockPosition(
@@ -329,7 +329,7 @@ describe('Board', () => {
             `,
         BUFFER_ROW_COUNT
       );
-      const tetromino = {
+      const tetromino: TetrominoLike = {
         color: Colors.GREEN,
         isLocked: true,
         getBlockPositions: mockGetBlockPosition(
@@ -361,7 +361,7 @@ describe('Board', () => {
        *   B B
        *   B
        */
-      const tetromino = {
+      const tetromino: TetrominoLike = {
         color: Colors.BLUE,
         isLocked: false,
         getBlockPositions: mockGetBlockPosition(
@@ -395,7 +395,7 @@ describe('Board', () => {
        *  B
        *  B
        */
-      const tetromino = {
+      const tetromino: TetrominoLike = {
         color: Colors.BLUE,
         isLocked: true,
         getBlockPositions: mockGetBlockPosition(
@@ -438,7 +438,7 @@ describe('Board', () => {
        *    G N N N . R R Y Y .
        *    G G . P P P P . Y Y
        */
-      const tetromino = {
+      const tetromino: TetrominoLike = {
         color: Colors.BLUE,
         isLocked: true,
         getBlockPositions: mockGetBlockPosition(
@@ -569,7 +569,7 @@ describe('Board', () => {
           bufferRowCount
         );
         const { blockOffsets, pivotPosition } = tetrominoInfo;
-        const tetromino = {
+        const tetromino: TetrominoLike = {
           color: Colors.BLUE,
           isLocked: true,
           getBlockPositions: mockGetBlockPosition(blockOffsets, pivotPosition),
@@ -724,7 +724,7 @@ describe('Board', () => {
       (_, tetrominoInfo, gridInfo) => {
         const board = Board.fromGrid(gridInfo.grid, gridInfo.bufferRowCount);
         const { blockOffsets, pivotPosition } = tetrominoInfo;
-        const tetromino = {
+        const tetromino: TetrominoLike = {
           color: Colors.BLUE,
           isLocked: true,
           getBlockPositions: mockGetBlockPosition(blockOffsets, pivotPosition),
